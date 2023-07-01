876c1ef0fb3d10145edc0a7057fa9210b2b00099
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-07-01 18:53:36 +0530

    Lazy Initialization

--- src/main/java/com/springboot/demo/mycoolapp/common/TennisCoach.java
+++ src/main/java/com/springboot/demo/mycoolapp/common/TennisCoach.java
@@ -1,11 +1,14 @@
 package com.springboot.demo.mycoolapp.common;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class TennisCoach implements Coach{
+
+    public TennisCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice backhand volley for 1 hour";
