f42cbad2e88be169fbb7a6a88b6ee56349115fbb
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-07-05 23:14:57 +0530

    Exception handling using @ExceptionHandler

--- REST CRUD API/src/main/java/com/restapi/crudapi/rest/StudentRestController.java
+++ REST CRUD API/src/main/java/com/restapi/crudapi/rest/StudentRestController.java
@@ -2,12 +2,12 @@ package com.restapi.crudapi.rest;
 
 import com.restapi.crudapi.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -36,7 +36,38 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId){
 
+        //exception handling
+        if(studentId < 0 || studentId >= theStudents.size()){
+            throw new StudentNotFoundException("Student id not found: " + studentId );
+        }
+
         //just index into the list
         return theStudents.get(studentId);
+    }
+
+    //Adding an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e){
+
+        //create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        //return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    //add another exception handler to catch any type of exceptions
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleAnyException(Exception e){
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        //return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
