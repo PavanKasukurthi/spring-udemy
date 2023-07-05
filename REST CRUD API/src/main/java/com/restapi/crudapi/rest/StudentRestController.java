9ba9e67469488eded881466bb09bb99c9bfaf651
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-07-05 23:32:40 +0530

    Global Exception Handling

--- REST CRUD API/src/main/java/com/restapi/crudapi/rest/StudentRestController.java
+++ REST CRUD API/src/main/java/com/restapi/crudapi/rest/StudentRestController.java
@@ -45,29 +45,5 @@ public class StudentRestController {
         return theStudents.get(studentId);
     }
 
-    //Adding an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e){
 
-        //create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(e.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    //add another exception handler to catch any type of exceptions
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleAnyException(Exception e){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(e.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
