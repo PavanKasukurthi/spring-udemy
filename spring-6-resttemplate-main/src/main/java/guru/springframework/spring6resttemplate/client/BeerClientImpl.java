e33b12a04bbb4aa7a8cb3b955371f0fb26e8aff9
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-08-03 16:39:28 +0530

    Spring rest template

---
+++ spring-6-resttemplate-main/src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -0,0 +1,33 @@
+package guru.springframework.spring6resttemplate.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.RestPageImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
+    @Override
+    public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<RestPageImpl> stringResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, RestPageImpl.class);
+
+
+        return null;
+    }
+}
